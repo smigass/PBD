226d7453607e3a760ca6c49fa1fbf1b228e9ae7b
Szymon Migas <ataszymon@gmail.com>
2024-12-01 20:16:40 +0100

    removed unused file

--- Kolosy/sql.sql
+++
@@ -1,7 +0,0 @@
-select distinct member.member_no, firstname, lastname, count(juvenile.adult_member_no)
-from member
-         left outer join loanhist on member.member_no = loanhist.member_no
-         left join juvenile on juvenile.adult_member_no = member.member_no
-where loanhist.member_no is null
-group by member.member_no, firstname, lastname
-order by 1
